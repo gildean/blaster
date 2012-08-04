5b80bf24cec9a056cf7ad6b1d8b370dbdf9dea75
gildean <gildean@salaliitto.com>
2012-08-04 12:16:27 +0300

    Blaster loaded!

--- blaster.js
+++ blaster.js
@@ -10,59 +10,60 @@ var options = {
   path: url.parse(getaddress).path,
 };
 
-var reset, bold, magenta, red, green, cyan, blue, white;
+var reset, bold, red, green, cyan, blue, white;
 reset = '\x1B[0m';
 bold = '\x1B[1m';
 red = '\x1B[31m';
 green = '\x1B[32m';
-magenta = '\x1B[35m';
 cyan = '\x1B[36m';
 blue = '\x1B[34m';
 white = '\x1B[37m';
 
 
 var startTime = Date.now();
-console.log('\r\n'+ bold +' ,___  _    ____ ____ ___ ____ ____ ');
+console.log('\r\n\r\n'+ bold +' ,___  _    ____ ____ ___ ____ ____ ');
 console.log(' ||__] |    |__| [__   |  |___ |__/');
 console.log(' ||__] |___ |  | ___]  |  |___ |  \\ ');
 console.log('  ' + cyan + '.\'.     .-.     .-.     .-.     .-.');
 console.log(white + '01001' + blue + '\\' + white + '01001o0' + blue + '\\' + white + 'o01001o' + blue + '\\' + white + '1o01001' + blue + '\\' + white + '01001o0' + blue + '\\');
 console.log(cyan + '\'     `-\'     `-\'     `-\'     `-\'');
-console.log('}>=<{ Target : '+ white + options.host);
-console.log(cyan + '}>=<{ Ammo   : '+ white + times);
-console.log(cyan + '}>=<{ Caliber: '+ white + blastercaliber);
+console.log('}>=<{ Target : '+ white + options.host + cyan + ' }');
+console.log(cyan + '}>=<{ Ammo   : '+ white + times + cyan + ' }');
+console.log(cyan + '}>=<{ Caliber: '+ white + blastercaliber + cyan + ' }');
 console.log('  ' + cyan + '.\'.     .-.     .-.     .-.     .-.');
 console.log(white + '01001' + blue + '\\' + white + '01001o0' + blue + '\\' + white + 'o01001o' + blue + '\\' + white + '1o01001' + blue + '\\' + white + '01001o0' + blue + '\\');
 console.log(cyan + '\'     `-\'     `-\'     `-\'     `-\'');
-console.log(cyan + '\r\n}>=<{ ' + white + 'CHARGING CELLS');
+process.stdout.write(cyan + '\r\n\r\n}>=<{ ' + white + 'Charging \x1B[s');
 
 
 function blaster(i) {
   
   var req = http.get(options, function(res) {
-    if (i%100===0) {
-      process.stdout.write(bold + cyan + '}>=<{ ' + white + 'Server returns fire: ' + green + 'STATUS ' + res.statusCode + '\r');
-    }
     if (i==times){
       function ok() {
-        console.log(cyan + '\r\n\r\n}>=<{ ' + white + 'Blasting lasted: ' + green + ((endTime - startTime)/1000) + ' s');
+        console.log(cyan + '\r\n\r\n\r\n\r\n}>=<{ ' + white + 'Blasting lasted ' + cyan + '}>=<{ ' + green + ((endTime - startTime)/1000) + ' s');
         console.log(cyan + '}>=<{ ' + white + 'OK!\r\n\r\n');
       };
       var endTime = Date.now();
       setTimeout(ok, (blastercaliber*15));
     }
+    if (i%100===0) {
+      process.stdout.write(cyan + '}>=<{ ' + white + 'Server returns fire ' + cyan + '}>=<{ ' + green + 'STATUS ' + res.statusCode + '\r');
+    }
   }).on('error', function(e) {
     console.log(red + '\r\n}>=<{ Critical hit: ' + white + e.message + '\r\n');
   });
 
-  process.stdout.write(cyan + '}>=<{ ' + reset + i + ' out of ' + times + ' cells charged' + cyan + bold + ' }\r');
+  process.stdout.write(cyan + '\x1B[u}>=<{ ' + reset + i + bold + ' / ' + reset + times + bold + ' cells' + cyan + '\r');
 
 };
 
-console.time('\r\n}>=<{ ' + white + 'Charging time'+green);
+
+console.time('\r\n}>=<{ ' + white + 'Charging time ' + cyan + '}>=<{' + green);
 for (i=1;i<=times;i++) {
   blaster(i);
 };
-console.timeEnd('\r\n}>=<{ ' + white + 'Charging time'+green);
+console.timeEnd('\r\n}>=<{ ' + white + 'Charging time ' + cyan + '}>=<{' + green);
 
-console.log(cyan + '\r\n}>=<{ ' +  white + 'Fully charged!\r\n' + cyan + '}>=<{ ' + white + 'Fire' + red + '!\r\n');
+
+process.stdout.write(cyan + '\r\n\r\n}>=<{ ' +  white + 'Fully charged! ' + cyan + '}>=<{ ' + red + 'Fire!\r\n\r\n');
