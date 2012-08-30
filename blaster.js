81020732684422bf2fa58ebc4569d85976d3ab62
gildean <gildean@salaliitto.com>
2012-08-30 07:03:09 +0300

    Fix ++

--- blaster.js
+++ blaster.js
@@ -1,19 +1,19 @@
-var http = require('http')
-  , url = require('url')
-  , getaddress = process.argv[2] || 'http://localhost'
-  , times = process.argv[3] || 100
-  , blastercaliber = process.argv[4] || 100
-  , reset = '\x1B[0m'
-  , bold = '\x1B[1m'
-  , red = '\x1B[31m'
-  , green = '\x1B[32m'
-  , cyan = '\x1B[36m'
-  , blue = '\x1B[34m'
-  , white = '\x1B[37m'
-  , options = {
-      host: url.parse(getaddress).hostname,
-      port: url.parse(getaddress).port,
-      path: url.parse(getaddress).path,
+var http = require('http'),
+    url = require('url'),
+    getaddress = process.argv[2] || 'http://localhost',
+    times = process.argv[3] || 100,
+    blastercaliber = process.argv[4] || 100,
+    reset = '\x1B[0m',
+    bold = '\x1B[1m',
+    red = '\x1B[31m',
+    green = '\x1B[32m',
+    cyan = '\x1B[36m',
+    blue = '\x1B[34m',
+    white = '\x1B[37m',
+    options = {
+        host: url.parse(getaddress).hostname,
+        port: url.parse(getaddress).port,
+        path: url.parse(getaddress).path,
     };
 
 // caliber sets the max sockets used for pooling requests
@@ -42,19 +42,19 @@ process.stdout.write(reset + cyan + '\r\n\r\n}>=<{ ' + white + bold + ' Charging
 console.time(reset + cyan + '\r\n}>=<{ ' + white + bold + ' Charging time    ' + reset + cyan + '}>=<{' + green + bold);
 // print out the counter on the first tick marking pooling of the connections
 // fire the requests on the second tick
-for (var i=1;i<=times;i++) {
+for (var i=0;i<=times;i += 1) {
   process.stdout.write(reset + cyan + '\x1B[u}>=<{' + white + ': ' + ((i/times)*100).toFixed(1) + ' %\r');
   blaster(i);
 };
 // mark the end of the first tick loop
 console.timeEnd(reset + cyan + '\r\n}>=<{ ' + white + bold + ' Charging time    ' + reset + cyan + '}>=<{' + green + bold);
 
-// the request function wrapped in a closure
+// the request function
 function blaster(i) {
   // pick up the saved cursor position and show the counter on first tick loop
   // the request which returns callback on second tick loop
   http.get(options, function(res) {
-    if (i==times) {
+    if (i == times) {
       process.nextTick(endmessage);
     }
     // print out all error messaages  
